7531351612766507a169631f8f19f60d2b435303
Parikshit Machwe <pmachwe@gmail.com>
2022-09-08 16:32:16 +0530

    leap indent guide etc

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -3,6 +3,10 @@ require('settings')
 require('mappings')
 require('nvim-cmp')
 require('themes/nightfox')
+require('lsp')
+require('treesitter')
 
 -- Set theme
 vim.cmd("colorscheme nightfox")
+
+require('leap').set_default_keymaps()
--- nvim-lua/lua/plugins/init.lua
+++ nvim-lua/lua/plugins/init.lua
@@ -14,6 +14,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
+  use 'quangnguyen30192/cmp-nvim-tags'
   use 'hrsh7th/nvim-cmp'
 
   -- For luasnip users.
@@ -24,6 +25,19 @@ return require('packer').startup(function(use)
   use "EdenEast/nightfox.nvim"
 
   -- LSP
+  use 'neovim/nvim-lspconfig'
+
   -- Treesitter
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+  }
+
+  -- General utilities
+  use 'ggandor/leap.nvim' -- sneak movement
+  use {
+      'nmac427/guess-indent.nvim',
+      config = function() require('guess-indent').setup {} end,
+  }
 
 end)
