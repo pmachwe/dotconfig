196fb71a36bd1b807a5000af3b84fcabcff8afc0
Parikshit Machwe <pmachwe@gmail.com>
2022-09-20 22:13:30 +0530

    Telescope customizations

--- nvim-lua/lua/my-telescope/init.lua
+++ nvim-lua/lua/my-telescope/init.lua
@@ -3,8 +3,8 @@ local opts = { noremap = true, silent = true }
 
 -- Telescope basics
 vim.api.nvim_set_keymap('n', "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
-vim.api.nvim_set_keymap('n', "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
-vim.api.nvim_set_keymap('n', "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
+vim.api.nvim_set_keymap('n', "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>", opts)
 vim.api.nvim_set_keymap('n', "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
 
 -- Find files in current directory
@@ -18,3 +18,34 @@ vim.api.nvim_set_keymap('n', '<Leader>fts', ":Telescope tmux sessions<CR>", opts
 local ts = require('telescope')
 ts.load_extension('neoclip')
 vim.api.nvim_set_keymap('n', '<Leader>fr', ":Telescope neoclip<CR>", opts)
+
+-- Telescope customization
+local actions = require("telescope.actions")
+
+require("telescope").setup {
+  defaults = {
+    mappings = {
+      i = {
+          -- C-g to quit Telescope
+          ["<C-g>"] = actions.close,
+          -- Clear prompt
+          ["<C-u>"] = false
+      },
+    },
+  }
+}
+
+M = {}
+
+-- Find Nvim config files
+M.find_nvim_config = function()
+    local opts = {}
+    opts.prompt_title = "Nvim Config"
+    opts.path_display = { "smart" }
+    opts.cwd = "~/.config/nvim"
+    require("telescope.builtin").find_files { opts }
+end
+
+vim.api.nvim_set_keymap('n', "<leader>fi", "<cmd>lua M.find_nvim_config()<CR>", opts)
+return M
+
