dc8fd6a918c0967ff0b136667a67fa70e6b0aadd
Parikshit Machwe <pmachwe@gmail.com>
2022-09-20 22:22:12 +0530

    adding fzf-native for telescope

--- nvim-lua/lua/my-plugins/init.lua
+++ nvim-lua/lua/my-plugins/init.lua
@@ -19,6 +19,9 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  -- Faster fuzzy impl in C
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
   -- Autocomplete
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
--- nvim-lua/lua/my-telescope/init.lua
+++ nvim-lua/lua/my-telescope/init.lua
@@ -19,6 +19,24 @@ local ts = require('telescope')
 ts.load_extension('neoclip')
 vim.api.nvim_set_keymap('n', '<Leader>fr', ":Telescope neoclip<CR>", opts)
 
+-- Fzf-native
+-- You dont need to set any of these options. These are the default ones. Only
+-- the loading is important
+-- ts.setup {
+--   extensions = {
+--     fzf = {
+--       fuzzy = true,                    -- false will only do exact matching
+--       override_generic_sorter = true,  -- override the generic sorter
+--       override_file_sorter = true,     -- override the file sorter
+--       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+--                                        -- the default case_mode is "smart_case"
+--     }
+--   }
+-- }
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+ts.load_extension('fzf')
+
 -- Telescope customization
 local actions = require("telescope.actions")
 
